cb63e68c4e5f0a7134955b9fccf1d28bb5249c26
Gary <gary@elasticdev.io>
2021-05-29 13:59:39 +0800

    update

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-export DOCKER_IMAGE=gear2000/jiffy-base:slim
-docker build -t $DOCKER_IMAGE .
+source ./build_params.txt
+#source /tmp/.credentials/docker/gear2000-token.txt 
+
+docker build -t $DOCKER_IMAGE . || exit 9
+
+echo "logging into docker"
+echo $DOCKER_TOKEN | docker login --username gear2000 --password-stdin || exit 9
+
 docker push $DOCKER_IMAGE 
