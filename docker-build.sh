638896f840d3288f1e27c57a4b3674a66f2c084b
Gary <gary@elasticdev.io>
2021-05-29 13:15:18 +0800

    update

--- docker-build.sh
+++ docker-build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 export DOCKER_IMAGE=gear2000/jiffy-base:builds.ubuntu.18.04
+export DOCKER_IMAGE_ALPINE=gear2000/jiffy-base:alpine
+#export DOCKER_IMAGE_SLIM=gear2000/jiffy-base:slim
+
 docker build -t $DOCKER_IMAGE .
 docker push $DOCKER_IMAGE 
 
-#export DOCKER_IMAGE2=gear2000/jiffy-base:slim
-#docker build -t $DOCKER_IMAGE2 . -f Dockerfile-slim
-#docker push $DOCKER_IMAGE2 
+docker build -t $DOCKER_IMAGE_ALPINE . -f Dockerfile-alpine
+docker push $DOCKER_IMAGE_ALPINE 
 
-export DOCKER_IMAGE3=gear2000/jiffy-base:alpine
-docker build -t $DOCKER_IMAGE3 . -f Dockerfile-alpine
-docker push $DOCKER_IMAGE3 
+#docker build -t $DOCKER_IMAGE_SLIM . -f Dockerfile-slim
+#docker push $DOCKER_IMAGE_SLIM 
